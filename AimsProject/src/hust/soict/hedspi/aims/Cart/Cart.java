88d9b1dc4a0008ca1984cb7c53761129f4e6e209
Tran Van Trung <trung.tv2417057@sis.hust.edu.vn>
2026-05-10 23:46:41 +0700

    Finish

---
+++ AimsProject/src/hust/soict/hedspi/aims/Cart/Cart.java
@@ -0,0 +1,62 @@
+package hust.soict.dsai.aims.Cart;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added.");
+        } else {
+            System.out.println("The cart is full.");
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+        for (DigitalVideoDisc dvd : dvdList) {
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                addDigitalVideoDisc(dvd); 
+            } else {
+                System.out.println("The cart is full. Cannot add more discs from the list.");
+                break;
+            }
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if (qtyOrdered + 2 <= MAX_NUMBERS_ORDERED) {
+            addDigitalVideoDisc(dvd1);
+            addDigitalVideoDisc(dvd2);
+        } else {
+            System.out.println("The cart is almost full. Cannot add both discs.");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc has been removed.");
+                return;
+            }
+        }
+        System.out.println("Disc not found.");
+    }
+
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
---
+++ AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -0,0 +1,24 @@
+package hust.soict.dsai.test.cart;
+
+import hust.soict.dsai.aims.Cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", "John Musker", 89, 18.99f);
+
+        System.out.println("--- Test them 2 DVD roi ---");
+        cart.addDigitalVideoDisc(dvd1, dvd2);
+
+        System.out.println("\n--- Test them mot mang DVD ---");
+        DigitalVideoDisc[] dvdList = {dvd3, new DigitalVideoDisc("Cinderella", "Animation", 15.5f)};
+        cart.addDigitalVideoDisc(dvdList);
+
+        System.out.println("\n--- Ket qua cuoi cung ---");
+        System.out.println("Total Cost: " + cart.totalCost());
+    }
+}
