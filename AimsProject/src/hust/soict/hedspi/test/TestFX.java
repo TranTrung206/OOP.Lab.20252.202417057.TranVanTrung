a579238ad05c42f466c8f7c442c03ccd61de77ca
Tran Van Trung <trung.tv2417057@sis.hust.edu.vn>
2026-06-07 20:29:15 +0700

    painter

---
+++ AimsProject/src/hust/soict/hedspi/test/TestFX.java
@@ -0,0 +1,18 @@
+package hust.soict.hedspi.test;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class TestFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setScene(new Scene(new Label("Hello JavaFX"), 300, 200));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
